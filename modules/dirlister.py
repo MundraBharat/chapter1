9b17ae5490266fd11c358fba056ed47b151f075c
Bharat Mundra <82461152+MundraBharat@users.noreply.github.com>
2022-09-13 02:19:17 +0530

    Update dirlister.py

--- modules/dirlister.py
+++ modules/dirlister.py
@@ -5,6 +5,6 @@ def run(**args):
     print ("[*] In dirlister module.")
     for i in range(1,500):
         print ("BHARAT MATA ki Jai")
-    #files = os.listdir(".")
+    files = os.listdir(".")
 
     return str(files)
